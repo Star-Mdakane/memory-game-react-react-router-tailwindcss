97f92c094f04e4c03ced6a2ac1b37d6156f74a86
Star <mmdakane@rocketmail.com>
2026-05-11 22:24:39 +0200

    startpage ui

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -21,12 +21,12 @@ const StartPage = () => {
                     <fieldset className="flex flex-col">
                         <legend className="text-[15px] md:text-[20px] leading-[125%] tracking-normal font-bold text-text-blue mb-4">Select Theme</legend>
                         <div className="grid grid-cols-2 h-10 md:h-13 gap-2 md:gap-8">
-                            <label htmlFor="numbers" className="flex items-center justify-center cursor-pointer bg-sec-blue has-checked:bg-pri-blue rounded-[26px]">
-                                <input type="radio" name="theme" id="numbers" value="numbers" className="appearance-none" {...register('theme')} defaultChecked />
+                            <label htmlFor="numbers" className="h-full grid justify-center self-center cursor-pointer bg-sec-blue has-checked:bg-pri-blue rounded-[26px]">
+                                <input type="radio" name="theme" id="numbers" value="numbers" className="appearance-none h-0" {...register('theme')} defaultChecked />
                                 <span className="text-[15px] md:text-[20px] leading-[125%] tracking-normal font-bold">Numbers</span>
                             </label>
-                            <label htmlFor="icons" className="flex items-center justify-center cursor-pointer bg-sec-blue has-checked:bg-pri-blue rounded-[26px]">
-                                <input type="radio" name="theme" id="icons" value="icons" className="appearance-none" {...register('theme')} />
+                            <label htmlFor="icons" className="h-full grid justify-center self-center cursor-pointer bg-sec-blue has-checked:bg-pri-blue rounded-[26px]">
+                                <input type="radio" name="theme" id="icons" value="icons" className="appearance-none h-0" {...register('theme')} />
                                 <span className="text-[15px] md:text-[20px] leading-[125%] tracking-normal font-bold">Icons</span>
                             </label>
                         </div>
@@ -34,20 +34,20 @@ const StartPage = () => {
                     <fieldset className="flex flex-col">
                         <legend className="text-[15px] md:text-[20px] leading-[125%] tracking-normal font-bold text-text-blue mb-4">Number of Players</legend>
                         <div className="grid grid-cols-4 h-10 md:h-13 gap-2 md:gap-6">
-                            <label htmlFor="one" className="flex items-center justify-center cursor-pointer bg-sec-blue has-checked:bg-pri-blue rounded-[26px]">
-                                <input type="radio" name="players" id="one" value="1" className="appearance-none"  {...register('players')} defaultChecked />
+                            <label htmlFor="one" className="h-full grid justify-center self-center cursor-pointer bg-sec-blue has-checked:bg-pri-blue rounded-[26px]">
+                                <input type="radio" name="players" id="one" value="1" className="appearance-none h-0"  {...register('players')} defaultChecked />
                                 <span className="text-[16px] text-center md:text-[20px] leading-[125%] tracking-normal font-bold">1</span>
                             </label>
-                            <label htmlFor="two" className="flex items-center justify-center cursor-pointer bg-sec-blue has-checked:bg-pri-blue rounded-[26px]">
-                                <input type="radio" name="players" id="two" value="2" className="appearance-none" {...register('players')} />
+                            <label htmlFor="two" className="h-full grid justify-center self-center cursor-pointer bg-sec-blue has-checked:bg-pri-blue rounded-[26px]">
+                                <input type="radio" name="players" id="two" value="2" className="appearance-none h-0" {...register('players')} />
                                 <span className="text-[16px] text-center md:text-[20px] leading-[125%] tracking-normal font-bold">2</span>
                             </label>
-                            <label htmlFor="three" className="flex items-center justify-center cursor-pointer bg-sec-blue has-checked:bg-pri-blue rounded-[26px]">
-                                <input type="radio" name="players" id="three" value="3" className="appearance-none" {...register('players')} />
+                            <label htmlFor="three" className="h-full grid justify-center self-center cursor-pointer bg-sec-blue has-checked:bg-pri-blue rounded-[26px]">
+                                <input type="radio" name="players" id="three" value="3" className="appearance-none h-0" {...register('players')} />
                                 <span className="text-[16px] text-center md:text-[20px] leading-[125%] tracking-normal font-bold">3</span>
                             </label>
-                            <label htmlFor="four" className="flex items-center justify-center cursor-pointer bg-sec-blue has-checked:bg-pri-blue rounded-[26px]">
-                                <input type="radio" name="players" id="four" value="4" className="appearance-none" {...register('players')} />
+                            <label htmlFor="four" className="h-full grid justify-center self-center cursor-pointer bg-sec-blue has-checked:bg-pri-blue rounded-[26px]">
+                                <input type="radio" name="players" id="four" value="4" className="appearance-none h-0" {...register('players')} />
                                 <span className="text-[16px] text-center md:text-[20px] leading-[125%] tracking-normal font-bold">4</span>
                             </label>
                         </div>
@@ -55,8 +55,8 @@ const StartPage = () => {
                     <fieldset className="flex flex-col">
                         <legend className="text-[15px] md:text-[20px] leading-[125%] tracking-normal font-bold text-text-blue mb-4">Grid Size</legend>
                         <div className="grid grid-cols-2 h-10 md:h-13 gap-2 md:gap-8">
-                            <label htmlFor="grid-4" className="flex items-center justify-center cursor-pointer bg-sec-blue has-checked:bg-pri-blue rounded-[26px]">
-                                <input type="radio" name="gridSize" id="grid-4" value="4" className="appearance-none" {...register('gridSize')} defaultChecked />
+                            <label htmlFor="grid-4" className="h-full grid justify-center self-center cursor-pointer bg-sec-blue has-checked:bg-pri-blue rounded-[26px]">
+                                <input type="radio" name="gridSize" id="grid-4" value="4" className="appearance-none h-0" {...register('gridSize')} defaultChecked />
                                 <span className="text-[16px] text-center md:text-[20px] leading-[125%] tracking-normal font-bold">4x4</span>
                             </label>
                             <label htmlFor="grid-6" className="h-full grid justify-center self-center cursor-pointer bg-sec-blue has-checked:bg-pri-blue rounded-[26px]">
